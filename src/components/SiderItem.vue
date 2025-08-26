e85f2117b5f9b243cf97c6ff57e8097be91074bf
CakeAL <1742507190@qq.com>
2025-08-26 19:35:46 +0800

    ✨ feat(Sider): add click

--- src/components/SiderItem.vue
+++ src/components/SiderItem.vue
@@ -1,12 +1,28 @@
 <script setup lang="ts">
 import dayjs from "dayjs";
+import { computed } from "vue";
+import { store } from "../store";
 const props = defineProps<{
   file: string;
   timestamp: number; // 秒
 }>();
+
+const setSelected = () => {
+  store.curItem = props.file;
+};
+
+const selected = computed(() => {
+  return store.curItem === props.file;
+});
 </script>
 <template>
-  <n-card hoverable class="sider-item" content-style="padding: 10px 15px;">
+  <n-card
+    hoverable
+    class="sider-item"
+    content-style="padding: 10px 15px;"
+    :class="{ selected: selected }"
+    @click="setSelected"
+  >
     {{ file }}
     <br />
     <span class="timestamp">{{
@@ -23,5 +39,9 @@ const props = defineProps<{
 .timestamp {
   font-size: 12px;
   color: #939393;
+}
+
+.selected {
+  background-color: #dfedd6;
 }
 </style>
--- src/layouts/Sider.vue
+++ src/layouts/Sider.vue
@@ -2,6 +2,22 @@
 import SiderItem from "../components/SiderItem.vue";
 </script>
 <template>
-  <SiderItem :file="'filename.m4a'" :timestamp="1756109360" />
+  <div class="container">
+    <SiderItem :file="'filename1.m4a'" :timestamp="1756109360" />
+    <SiderItem :file="'filename2.m4a'" :timestamp="1756109360" />
+    <SiderItem :file="'filename3.m4a'" :timestamp="1756109360" />
+    <SiderItem :file="'filename4.m4a'" :timestamp="1756109360" />
+    <SiderItem :file="'filename5.m4a'" :timestamp="1756109360" />
+    <SiderItem :file="'filename6.m4a'" :timestamp="1756109360" />
+    <SiderItem :file="'filename7.m4a'" :timestamp="1756109360" />
+    <SiderItem :file="'filename8.m4a'" :timestamp="1756109360" />
+    <SiderItem :file="'filename9.m4a'" :timestamp="1756109360" />
+    <SiderItem :file="'filename10.m4a'" :timestamp="1756109360" />
+  </div>
 </template>
-<style scoped></style>
+<style scoped>
+.container {
+  height: 100vh;
+  overflow-y: auto;
+}
+</style>
